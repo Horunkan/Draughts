63b840cdafe6f16c204c19a38003b80f1d3b2f68
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-03 10:56:24 +0200

    Initial

---
+++ desktop/src/com/Horunkan/Draughts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.Horunkan.Draughts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.Horunkan.Draughts.Draughts;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Draughts(), config);
+	}
+}
