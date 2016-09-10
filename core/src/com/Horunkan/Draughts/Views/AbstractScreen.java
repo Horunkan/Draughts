d6ce2c159ff4462d4f0b769e45b8060297f94566
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-10 09:48:15 +0200

    Fix screen input

--- core/src/com/Horunkan/Draughts/Views/AbstractScreen.java
+++ core/src/com/Horunkan/Draughts/Views/AbstractScreen.java
@@ -17,7 +17,6 @@ public abstract class AbstractScreen implements Screen {
 		createCamera();
 		
 		stage = new Stage(new StretchViewport(Draughts.WIDTH, Draughts.HEIGHT, camera));
-		Gdx.input.setInputProcessor(stage);	
 		spriteBatch = new SpriteBatch();
 	}
 	
@@ -27,7 +26,7 @@ public abstract class AbstractScreen implements Screen {
 		camera.update();
 	}
 		
-	@Override public void show() { }
+	@Override public void show() { Gdx.input.setInputProcessor(stage); }
 	@Override public void resize(int width, int height) { }
 	@Override public void pause() { }
 	@Override public void resume() { }
