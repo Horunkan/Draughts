333e7becaf3148d1467ff73327269550476ece3b
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-10 09:46:56 +0200

    GameScreen class

---
+++ core/src/com/Horunkan/Draughts/Views/GameScreen.java
@@ -0,0 +1,14 @@
+package com.Horunkan.Draughts.Views;
+
+public class GameScreen extends AbstractScreen {
+
+	public GameScreen() {
+		super();
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+}
