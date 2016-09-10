80929f525e726fec6ed551eaa0ad8712b88e0803
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-10 09:55:35 +0200

    Back to main menu from game

--- core/src/com/Horunkan/Draughts/Views/GameScreen.java
+++ core/src/com/Horunkan/Draughts/Views/GameScreen.java
@@ -2,6 +2,7 @@ package com.Horunkan.Draughts.Views;
 
 import com.Horunkan.Draughts.Draughts;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 
 public class GameScreen extends AbstractScreen {
@@ -15,6 +16,8 @@ public class GameScreen extends AbstractScreen {
 		//Clear view
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) game.setScreen(Draughts.ScreenMode.MAIN_MENU);
 		
 		stage.draw();
 	}
