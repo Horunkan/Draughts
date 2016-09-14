f1d2442a4a1ceec9fee8d934be650756d8c783ed
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-14 09:28:34 +0200

    Add pawn position in DrawPawn

--- core/src/com/Horunkan/Draughts/Game/GUI/DrawPawn.java
+++ core/src/com/Horunkan/Draughts/Game/GUI/DrawPawn.java
@@ -6,14 +6,14 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class DrawPawn extends Image {
-	public DrawPawn(Skin skin, int pawnType) {
+	public DrawPawn(Skin skin, int pawnType, int posX, int posY) {
 		if(pawnType == 2) this.setDrawable(skin, "pawnBright");
 		else if(pawnType == 3) this.setDrawable(skin, "pawnDark");
 		this.setSize(65, 65);
 		
 		this.addListener(new InputListener() {
 	        public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-	        	System.out.println("Pressed pawn");
+	        	System.out.println("Pressed pawn on position: [" + posX + "," + posY + "]");
                 return false;
 	        }
 		});
