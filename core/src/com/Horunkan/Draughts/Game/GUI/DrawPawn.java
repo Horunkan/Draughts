6cc965674a8640cc09edec931c84379ca273e7e2
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-14 09:25:57 +0200

    Input listener in DrawPawn

--- core/src/com/Horunkan/Draughts/Game/GUI/DrawPawn.java
+++ core/src/com/Horunkan/Draughts/Game/GUI/DrawPawn.java
@@ -1,5 +1,7 @@
 package com.Horunkan.Draughts.Game.GUI;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
@@ -8,5 +10,12 @@ public class DrawPawn extends Image {
 		if(pawnType == 2) this.setDrawable(skin, "pawnBright");
 		else if(pawnType == 3) this.setDrawable(skin, "pawnDark");
 		this.setSize(65, 65);
+		
+		this.addListener(new InputListener() {
+	        public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+	        	System.out.println("Pressed pawn");
+                return false;
+	        }
+		});
 	}
 }
