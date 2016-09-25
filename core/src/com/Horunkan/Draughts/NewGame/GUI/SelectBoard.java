e20d07f840ad08c097c5a8d9fc034799178c3f62
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-25 18:08:50 +0200

    Select board

---
+++ core/src/com/Horunkan/Draughts/NewGame/GUI/SelectBoard.java
@@ -0,0 +1,92 @@
+package com.Horunkan.Draughts.NewGame.GUI;
+
+import com.Horunkan.Draughts.Utilities.FontLoader;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public class SelectBoard {
+	private final int titleFontSize = 25;
+	private final int listFontSize = 15;
+
+	private LabelStyle titleStyle;
+	private Label title;
+	private ListStyle listStyle;
+	private List<String> boardList;
+	private ScrollPaneStyle scrollStyle;
+	private ScrollPane boardsScroller;
+	
+	public SelectBoard() {
+		createTitle();
+		createListStyle();
+		loadList();
+		createScrollPaneStyle();
+		createScrollPane();
+	}
+	
+	public void addToTable(Table tab) {
+		tab.add(title).row();
+		tab.add(boardsScroller).width(250).height(250);
+	}
+	
+	private void createTitle() {
+		titleStyle = new LabelStyle();
+		titleStyle.font = FontLoader.getInstance().getFont(titleFontSize);
+		titleStyle.fontColor = Color.WHITE;
+		
+		title = new Label("Select board", titleStyle);
+	}
+	
+	private void createListStyle() {
+		Pixmap selectionPixmap = new Pixmap(100, 100, Format.RGBA8888);
+		selectionPixmap.setColor(Color.DARK_GRAY);
+		selectionPixmap.fill();
+		
+		listStyle = new ListStyle();
+		listStyle.font = FontLoader.getInstance().getFont(listFontSize);
+		listStyle.fontColorUnselected = Color.WHITE;
+		listStyle.fontColorSelected = Color.WHITE;
+		listStyle.selection = new TextureRegionDrawable(new TextureRegion(new Texture(selectionPixmap)));
+	}
+	
+	private void loadList() {
+		boardList = new List<String>(listStyle);
+		FileHandle directory = Gdx.files.internal("Boards");
+		String[] buffer = new String[directory.list().length];
+
+		for(int i = 0; i < directory.list().length; ++i) buffer[i] = directory.list()[i].name();
+		boardList.setItems(buffer);
+	}
+	
+	private void createScrollPaneStyle() {
+		Pixmap scrollKnobPixmap = new Pixmap(15, 15, Format.RGBA8888);
+		scrollKnobPixmap.setColor(Color.DARK_GRAY);
+		scrollKnobPixmap.fill();
+		
+		Pixmap scrollPixmap = new Pixmap(15, 15, Format.RGBA8888);
+		scrollPixmap.setColor(Color.LIGHT_GRAY);
+		scrollPixmap.fill();
+		
+		scrollStyle = new ScrollPaneStyle();
+		scrollStyle.vScrollKnob = new TextureRegionDrawable(new TextureRegion(new Texture(scrollKnobPixmap)));
+		scrollStyle.vScroll = new TextureRegionDrawable(new TextureRegion(new Texture(scrollPixmap)));
+	}
+	
+	private void createScrollPane() {
+		boardsScroller = new ScrollPane(boardList, scrollStyle);
+		boardsScroller.setOverscroll(false, false);
+		boardsScroller.setFadeScrollBars(false);
+	}
+}
--- core/src/com/Horunkan/Draughts/Views/NewGame.java
+++ core/src/com/Horunkan/Draughts/Views/NewGame.java
@@ -1,6 +1,7 @@
 package com.Horunkan.Draughts.Views;
 
 import com.Horunkan.Draughts.Draughts;
+import com.Horunkan.Draughts.NewGame.GUI.SelectBoard;
 import com.Horunkan.Draughts.NewGame.GUI.SelectMode;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 public class NewGame extends AbstractScreen {
 	private Table mainContainer;
 	private SelectMode selectMode;
+	private SelectBoard selectBoard;
 
 	public NewGame(Draughts game) {
 		super(game);
@@ -19,6 +21,10 @@ public class NewGame extends AbstractScreen {
 		
 		selectMode = new SelectMode();
 		selectMode.addToTable(mainContainer);
+		mainContainer.row();
+		
+		selectBoard = new SelectBoard();
+		selectBoard.addToTable(mainContainer);
 			
 		stage.addActor(mainContainer);
 	}
@@ -29,6 +35,7 @@ public class NewGame extends AbstractScreen {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 		stage.draw();
 	}
 	
