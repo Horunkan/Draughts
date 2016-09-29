946edf3a12342a23aa4564807c1629c6c49f6316
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-09-29 16:06:49 +0200

    Check if textures were loaded

--- core/src/com/Horunkan/Draughts/Utilities/TextureLoader.java
+++ core/src/com/Horunkan/Draughts/Utilities/TextureLoader.java
@@ -36,14 +36,47 @@ public class TextureLoader {
 	}
 	
 	private TextureLoader() {
-		atlas = new TextureAtlas(Gdx.files.internal("textures.atlas"));		
+		try {
+			atlas = new TextureAtlas(Gdx.files.internal("textures.atlas"));		
+		}
+		catch(Exception ex) {
+			System.err.println("Failed to load textures - " + ex.getMessage());
+			ErrorCreateAtlas();
+		}
+		finally {
+			createSkin();
+		}
+	}
+	
+	private void ErrorCreateAtlas() {
+		Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
+		pixmap.setColor(Color.LIGHT_GRAY);
+		pixmap.fill();
+		TextureRegion region = new TextureRegion(new Texture(pixmap));
+		pixmap.dispose();
+		
+		atlas = new TextureAtlas();	
+		atlas.addRegion("buttonStandard", region);
+		atlas.addRegion("buttonPressed", region);
+		atlas.addRegion("boardCell_bright", region);
+		atlas.addRegion("boardCell_dark", region);
+		atlas.addRegion("pawn_bright", region);
+		atlas.addRegion("pawn_dark", region);
+		atlas.addRegion("pawn_bright_king", region);
+		atlas.addRegion("pawn_dark_king", region);
+		atlas.addRegion("checkboxOn", region);
+		atlas.addRegion("checkboxOff", region);
+		atlas.addRegion("NewGame_background", region);
+	}
+	
+	private void createSkin() {
 		skin = new Skin();
 		skin.addRegions(atlas);
 		skin.getRegion("pawn_bright").getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 		skin.getRegion("pawn_dark").getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 		skin.getRegion("pawn_bright_king").getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 		skin.getRegion("pawn_dark_king").getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-
+		
 		skin.add("buttonStandard", atlas.createSprite("buttonStandard"));
 		skin.add("buttonPressed", atlas.createSprite("buttonPressed"));
 		skin.add("boardBright", atlas.createSprite("boardCell_bright"));
